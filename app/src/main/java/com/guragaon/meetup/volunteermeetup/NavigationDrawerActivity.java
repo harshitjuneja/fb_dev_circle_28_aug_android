1a76864a4fca967f10b0b87ed818e4ecc6e95fae
Abhishek Singhania <abhisinghania14@gmail.com>
2016-08-28 16:29:55 +0530

    WalkInPage

--- app/src/main/java/com/guragaon/meetup/volunteermeetup/NavigationDrawerActivity.java
+++ app/src/main/java/com/guragaon/meetup/volunteermeetup/NavigationDrawerActivity.java
@@ -80,22 +80,15 @@ public class NavigationDrawerActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_qrcode) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_walkin) {
 
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+
     }
 }
--- app/src/main/java/com/guragaon/meetup/volunteermeetup/WalkInFragment.java
+++ app/src/main/java/com/guragaon/meetup/volunteermeetup/WalkInFragment.java
@@ -3,6 +3,8 @@ package com.guragaon.meetup.volunteermeetup;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,15 +16,25 @@ import android.view.ViewGroup;
 public class WalkInFragment extends Fragment
 {
 
-    public WalkInFragment() {
-        // Required empty public constructor
+    RecyclerView eventsList;
+    LinearLayoutManager linearLayoutManager;
+
+    public WalkInFragment()
+    {
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_walk_in, container, false);
+        View view= inflater.inflate(R.layout.fragment_walk_in, container, false);
+        eventsList=(RecyclerView) view.findViewById(R.id.eventList);
+        linearLayoutManager=new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false);
+        eventsList.setLayoutManager(linearLayoutManager);
+
+
+
+        return view;
     }
 
 }
